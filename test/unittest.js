b38b3415630077de17ce27590233d74fd5649f23
Anthony Kirby <anthony@anthony.org>
2018-05-20 22:42:52 +0100

    add test for FOpts

--- test/unittest.js
+++ test/unittest.js
@@ -7,4 +7,5 @@ describe("unit tests", function () {
     require('./test_decrypt.js')();
     require('./test_keygen.js')();
     require('./test_github_issue17.js')();
+    require('./test_fopts.js')();
 });
